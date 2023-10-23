0eb6768a8c6140cd00557c54ed48444b8807798d
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-10-23 16:36:03 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import sys
 
 # Get the absolute path of the current file
 file_path = Path(__file__).resolve()
-
+print(file_path)
 # Get the parent directory of the current file
 root_path = file_path.parent
 
